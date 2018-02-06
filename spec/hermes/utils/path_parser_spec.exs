3551656dcf02200cad1acf18bfa646326d3aae0d
Peter Lejeck <peter.lejeck@gmail.com>
2018-02-06 13:45:06 -0800

    REformat PathParserSpec

--- spec/hermes/utils/path_parser_spec.exs
+++ spec/hermes/utils/path_parser_spec.exs
@@ -4,23 +4,23 @@ defmodule Hermes.Utils.PathParserSpec do
 
   describe ".parse(url)" do
     context "for /users/1/relationships/likes" do
-      subject(do: PathParser.parse("/users/1/relationships/likes"))
-      it(do: should(eq({:relationship, {"users", "1", "likes"}})))
+      subject do: PathParser.parse("/users/1/relationships/likes")
+      it do: should eq {:relationship, {"users", "1", "likes"}}
     end
 
     context "for /users/1/likes" do
-      subject(do: PathParser.parse("/users/1/likes"))
-      it(do: should(eq({:related, {"users", "1", "likes"}})))
+      subject do: PathParser.parse("/users/1/likes")
+      it do: should eq {:related, {"users", "1", "likes"}}
     end
 
     context "for /users/1" do
-      subject(do: PathParser.parse("/users/1"))
-      it(do: should(eq({:instance, {"users", "1"}})))
+      subject do: PathParser.parse("/users/1")
+      it do: should eq {:instance, {"users", "1"}}
     end
 
     context "for /users" do
-      subject(do: PathParser.parse("/users"))
-      it(do: should(eq({:model, {"users"}})))
+      subject do: PathParser.parse("/users")
+      it do: should eq {:model, {"users"}}
     end
   end
 end
